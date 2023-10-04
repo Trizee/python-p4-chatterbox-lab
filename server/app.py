455b0d05acfae7ffc070d7fe1318404329a8230d
Tri Hoang <trihoang09@gmail.com>
2023-10-04 15:35:00 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,90 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ["GET",'POST'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at).all()
+    messages_serialized = [message.to_dict() for message in messages]
+    if request.method == 'GET':
 
-@app.route('/messages/<int:id>')
+        reponse = make_response(
+            jsonify(messages_serialized),
+            200
+        )
+        reponse.headers['Content-Type'] = 'application/json'
+        return reponse
+
+    elif request.method == 'POST':
+
+        data = request.get_json()
+
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        ) 
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        reponse = make_response(
+            jsonify(new_message_dict),
+            201
+        )
+
+        reponse.headers['Content-Type'] = 'application/json'
+        return reponse
+
+@app.route('/messages/<int:id>', methods = ['GET','PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id = id).first()
+
+    if request.method == 'GET':
+
+        messages_dict = message.to_dict()
+
+        reponse = make_response(
+            jsonify(messages_dict),
+            200
+        )
+        reponse.headers['Content-Type'] = 'application/json'
+        return reponse
+    
+    elif request.method == 'PATCH':
+
+        data = request.get_json()
+
+        for attr in data:
+            setattr(message,attr,data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        reponse = make_response(
+            jsonify(message_dict),
+            200
+        )
+
+        return reponse
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."
+        }
+
+        reponse = make_response(
+            response_body,
+            200
+        )
+        reponse.headers['Content-Type'] = 'application/json'
+        return reponse
 
 if __name__ == '__main__':
     app.run(port=5555)
